343c85ce30d76b50b2755e8ca356274ef4b83fd0
Yodelyoda <nv.serhiy@gmail.com>
2020-10-23 13:45:58 +0300

    lab2.2

---
+++ lab2.1.cpp
@@ -0,0 +1,24 @@
+// lab2.1.cpp
+// <Новосільський Сергій>
+// Лабораторна робота No 2.1
+// Лінійні програми.
+// Варіант 17
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double m; // вхідний параметр
+
+
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	z1 = (sqrt(pow((3 * m + 2),2)) - 24 * m) / (3 * sqrt(m) - 2 / sqrt(m));
+	//z2 = sqrt(m);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
